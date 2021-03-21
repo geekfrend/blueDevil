6150bee222de00ebb8e411b4648a79b9fc209edc
Ravikumar Devarajan <ravikumar79@gmail.com>
2021-03-21 09:44:17 -0700

    added oled for both sides. trying to fix layer navigation

--- 5x7_blue/keymaps/blue/keymap.c
+++ 5x7_blue/keymaps/blue/keymap.c
@@ -10,6 +10,12 @@
 #define _FN     1
 #define _NUMPAD 2
 
+//layer macros
+#define TOQWERTY TG(_QWERTY)
+#define TOFN TG(_FN)
+#define TONUM TG(_NUMPAD)
+
+
 // Some basic macros
 #define TASK    LCTL(LSFT(KC_ESC))
 #define TAB_R   LCTL(KC_TAB)
@@ -35,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                        KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_DEL,
                                          KC_DEL,   KC_ENT,
                        KC_DOWN, KC_UP,
-    TG(1), TG(2),
+    TOFN, TONUM,
     KC_VOLD, KC_VOLU
     ),
 
@@ -46,18 +52,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    _______,   _______,   KC_LEFT,    KC_DOWN,  KC_RGHT,   _______,  _______,
    _______,   _______,   _______,   _______,   _______,   _______,
                          _______,   _______,
-                               _______, _______,
-                               _______, _______,
-                               _______, _______,
+                                    _______, _______,
+                                                          _______, _______,
+                                                          _______, _______,
         // right hand
                      KC_F7,     KC_F8,     KC_F9,     KC_F10,    KC_F11,    KC_F12,    _______,
                      _______,   _______,   _______,   _______,   _______,   _______,   _______,
                      _______,   _______,   _______,   _______,   _______,   KC_UP,     _______,
                                 _______,   _______,   _______,   KC_LEFT,   KC_DOWN,   KC_RIGHT,
-                                                      _______,   _______,
-        _______, _______,
-        TG(0),   TG(2),
-        _______, _______),
+                                           _______,   _______,
+                                _______, _______,
+   TOQWERTY, TONUM,
+   _______, _______
+   ),
 
 [_NUMPAD] = LAYOUT_5x7(
   // left hand
@@ -66,23 +73,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    _______,   _______,   _______,   _______,   _______,   _______,  _______,
    _______,   _______,   _______,   _______,   _______,   RESET,
                          _______,   _______,
-                               _______, _______,
-                               _______, _______,
-                               _______, _______,
+                                    _______, _______,
+                                                          _______, _______,
+                                                          _______, _______,
         // right hand
                      _______,   _______,   KC_NLCK,   _______,   KC_PMNS,   KC_PPLS,   _______,
                      _______,   _______,   KC_P7,     KC_P8,     KC_P9,     _______,   _______,
                      _______,   _______,   KC_P4,     KC_P5,     KC_P6,     KC_PAST,   _______,
                                 _______,   KC_P1,     KC_P2,     KC_P3,     KC_PSLS,   _______,
-                                                      KC_P0,     KC_PDOT,
-        _______, _______,
-        TG(0),   TG(1),
-        _______, _______),
+                                           KC_P0,     KC_PDOT,
+                                _______, _______,
+    TOQWERTY,TOFN,
+    _______, _______
+    )
 
 };
 
 #ifdef OLED_DRIVER_ENABLE
 void oled_task_user(void) {
+    if (is_keyboard_master()) {
+        render_logo();  // Renders a static logo
+        oled_scroll_left();  // Turns on scrolling
+    } else {
     // Host Keyboard Layer Status
     oled_write_P(PSTR("Layer: "), false);
 
@@ -106,5 +118,16 @@ void oled_task_user(void) {
     oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
     oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
     oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
+  }
 }
 #endif
+
+static void render_logo(void) {
+    static const char PROGMEM qmk_logo[] = {
+        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94,
+        0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4,
+        0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00
+    };
+
+    oled_write_P(qmk_logo, false);
+}
