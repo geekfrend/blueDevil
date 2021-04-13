92b735def697a536a2ba5170117729d93ac34975
Ravikumar Devarajan <ravikumar79@gmail.com>
2021-04-13 13:23:32 -0700

    some keymap changes

--- 5x7_blue/keymaps/blue/keymap.c
+++ 5x7_blue/keymaps/blue/keymap.c
@@ -24,19 +24,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_TAB,    KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   KC_LBRC,
    KC_LSFT,   KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_PGUP,
    KC_LCTL,   KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,
-                       KC_LCMD, KC_SPC,
-                                        KC_LEFT,  KC_RGHT,
-                                                        KC_LOPT, KC_ESC,
-                                                        LCAG(KC_ENT),KC_MUTE,
+                       TG(_FN), TG(_NUMPAD),
+                                        KC_SPC,  KC_LOPT,
+                                                        KC_MUTE, KC_LCMD,
+                                                        KC_DEL,  KC_ESC,
         // right hand
               KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,
               KC_RBRC, KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_BSLS,
               KC_PGDN, KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
-                       KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_DEL,
-                                         KC_SPC,   KC_ENT,
-                       KC_UP,   KC_DOWN,
-    TG(_FN), TG(_NUMPAD),
-    KC_VOLD, KC_VOLU
+                       KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
+                                         KC_VOLD,   KC_VOLU,
+                       KC_DOWN,   KC_RGHT,
+    KC_UP,   KC_SPC,
+    KC_LEFT, KC_ENT
     ),
 
 [_FN] = LAYOUT_5x7(
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    _______,   _______,   _______,    _______,  _______,   _______,  _______,
    KC_CAPS,   _______,   _______,    _______,  _______,   _______,  _______,
    _______,   _______,   _______,   _______,   _______,   _______,
-                         _______,   _______,
+                         TG(_FN),   TG(_NUMPAD),
                                     _______, _______,
                                                           _______, _______,
                                                           _______, _______,
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                 _______,   _______,   _______,   _______,   _______,   _______,
                                            _______,   _______,
                                 _______, _______,
-   TG(_FN), TG(_NUMPAD),
+   _______, _______,
    _______, _______
    ),
 
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    _______,   _______,   _______,   _______,   _______,   _______,  _______,
    _______,   _______,   _______,   _______,   _______,   _______,  _______,
    _______,   _______,   _______,   _______,   _______,   _______,
-                         _______,   _______,
+                         KC_NO,    TG(_NUMPAD),
                                     _______, _______,
                                                           _______, _______,
                                                           _______, _______,
@@ -77,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                 _______,   KC_P1,     KC_P2,     KC_P3,     KC_PSLS,   _______,
                                            KC_P0,     KC_PDOT,
                                 _______, _______,
-    KC_NO, TG(_NUMPAD),
+    _______, _______,
     _______, _______
     )
 
